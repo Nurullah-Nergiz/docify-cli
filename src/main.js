a1a1fb9b69baa54d00230d1b3873cc527d0b2868
Nurullah-Nergiz <developer@nurullah.aleeas.com>
2023-12-10 11:45:23 +0300

    demo

--- src/main.js
+++ src/main.js
@@ -1,13 +1,1 @@
-import inquirer from "inquirer";
-import questions from "./questions/default.js";
-import writeFile from "./utils/writeFile.js";
-
-async function main() {
-   console.clear();
-   console.log("Cli Readme Generator App");
-
-   const answers = await inquirer.prompt(questions);
-   writeFile(answers);
-}
-
-main();
+console.log("app start");
