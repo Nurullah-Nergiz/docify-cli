3be45d89e499d22994e47334dee9e4fbb44c3d46
Nurullah-Nergiz <nurullah.devs@gmail.com>
2024-08-24 23:04:13 +0300

    Added Yargs js: added cache endpoint

--- src/libs/setConfig.js
+++ src/libs/setConfig.js
@@ -4,3 +4,7 @@ import config from '../utils/conf.js';
 export const setConfig = ({key,val}) => {
   config.set(key,val)
 };
+
+export const clearConfig = () => {
+  config.clear();
+};
--- src/main.js
+++ src/main.js
@@ -1,31 +1,60 @@
 import chalk from 'chalk'
 import prompts from 'prompts'
+import yargs from 'yargs/yargs'
+import { hideBin } from 'yargs/helpers'
+
 import questions from './questions/index.js'
 import createReadmeFile from './utils/writeReadmeFile.js'
-
 import checkUpdate from './libs/checkUpdate.js'
-import { setConfig } from './libs/setConfig.js'
+import { clearConfig, setConfig } from './libs/setConfig.js'
 
 // App init
 export default async function init() {
    console.clear()
-   console.log(chalk.hex('#1e90ff')('Docify - Documentation Generation Tool'))
-
    await checkUpdate()
 
-   // console.log(config.get().usage)
-
-   // @ts-ignore
-   const res = await prompts(questions, {
-      onCancel: () => {
-         process.exit()
-      },
+   yargs(hideBin(process.argv))
       // @ts-ignore
-      onSubmit: ({ name }, val) => {
-         setConfig({ key: name, val })
-      },
-   })
+      .command({
+         command: '$0',
+         desc: '',
+         // @ts-ignore
+         handler: async (argv) => {
+            console.log(
+               chalk.hex('#1e90ff')('Docify - Documentation Generation Tool'),
+            )
+            //@ts-ignore
+            const res = await prompts(questions, {
+               onCancel: () => {
+                  process.exit()
+               },
+               // @ts-ignore
+               onSubmit: ({ name }, val) => {
+                  setConfig({ key: name, val })
+               },
+            })
 
-   console.clear()
-   createReadmeFile(Object.values(res).filter((ans) => ans?.trim() != ''))
+            console.clear()
+            createReadmeFile(
+               Object.values(res).filter((ans) => ans?.trim() != ''),
+            )
+         },
+      })
+      // @ts-ignore
+      .command({
+         command: ['cache', 'clear'],
+         desc: 'cache data is cleared',
+         // @ts-ignore
+         handler: (argv) => {
+            // @ts-ignore
+            clearConfig()
+            console.log('cache')
+         },
+      })
+      .version()
+      .alias('v', 'version')
+      .help('h')
+      .alias('h', 'help')
+      .demandCommand(1, 'Komut eksik, varsayılan komut çalıştırılıyor.')
+      .parse()
 }
